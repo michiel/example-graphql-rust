996bb5116bb80b55a6fa27184e9363d12a7943b1
Michiel Kalkman <michiel@nosuchtype.com>
2018-04-08 22:31:26 +1000

    Connection

--- src/graphql_schema.rs
+++ src/graphql_schema.rs
@@ -5,6 +5,23 @@ use super::models::{User, NewUser};
 use ::graphql_driver::GraphQLExecutor;
 use ::database_queries::{db_create_user, db_find_user_by_uuid, db_find_users};
 
+#[derive(GraphQLObject)]
+#[graphql(description = "Page info")]
+pub struct PageInfo {
+    pub startCursor: String,
+    pub endCursor: String,
+    pub hasNextPage: bool,
+}
+
+#[derive(GraphQLObject)]
+#[graphql(description = "Connection")]
+pub struct UserConnection {
+    pub totalCount: Option<i32>,
+    pub edges: Vec<User>,
+    pub pageInfo: PageInfo,
+    pub cursor: String,
+}
+
 pub struct QueryRoot;
 
 graphql_object!(QueryRoot: GraphQLExecutor |&self| {
@@ -18,7 +35,23 @@ graphql_object!(QueryRoot: GraphQLExecutor |&self| {
                 ) -> FieldResult<Vec<User>> {
         let conn = executor.context().db_pool.get().unwrap();
         let limit = limit.unwrap_or(20) as i64;
-        Ok(db_find_users( &conn, limit).unwrap())
+        Ok(db_find_users(&conn, limit).unwrap())
+    }
+
+    field usersConnection(&executor) -> FieldResult<UserConnection> {
+        let conn = executor.context().db_pool.get().unwrap();
+        Ok(
+            UserConnection {
+                totalCount: Some(5),
+                edges: db_find_users(&conn, 5).unwrap(),
+                pageInfo: PageInfo {
+                    startCursor: "123".to_owned(),
+                    endCursor: "123".to_owned(),
+                    hasNextPage: true,
+                },
+                cursor: "123".to_owned(),
+            }
+        )
     }
 });
 
